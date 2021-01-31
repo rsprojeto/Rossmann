08910e36dc0ad0c1336d9c5cefd98d3b49912bc6
rsprojeto <rsprojeto3d@gmail.com>
2021-01-31 09:14:46 -0300

    update package Rossmann

--- inst/app.R
+++ inst/app.R
@@ -4,8 +4,10 @@ library(Rossmann)
 #r = plumb("plumber_api.R")
 #r$run(port = 5000, host = "0.0.0.0")
 
-server <- plumb(system.file('plumber.R',package = "Rossmann"))
+#server <- plumb(system.file('plumber.R',package = "Rossmann"))
 # server <- plumb('plumber.R')
-server$run(host = '0.0.0.0',port = 5000,swagger = T)
+#server$run(host = '0.0.0.0',port = 5000,swagger = T)
+
+api <- Rossmann::run_api()
 
 
